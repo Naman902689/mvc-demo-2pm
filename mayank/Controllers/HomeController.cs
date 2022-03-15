e5aba4c52ce4401d5c795d7c573123d40b044953
naman <shuklanama409@gmail.com>
2022-03-15 13:21:06 +0530

    Initial commit

---
+++ mayank/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using mayank.dbase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mayank.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            MCAEntities obj = new MCAEntities();
+            var res = obj.courses.ToList();
+            return View(res);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
